c2084beed4578a711f60bd9c85d59ff7451281b2
w1llmccray342 <william.mccray@g.austincc.edu>
2024-06-25 19:00:48 +0000

    commit game code in main() for question_b

--- src/question_b/main.py
+++ src/question_b/main.py
@@ -1,21 +1,40 @@
 #add import
 import question_b
+import question_b.question_b
+
+def game():
+
+    hidden_number = question_b.get_random_number
+
+    my_guess = input("Please enter any number 1-5: ")
+
+    while my_guess != hidden_number:
+
+        if my_guess == hidden_number:
+            print("You win!")
+            return
+
+        elif my_guess != hidden_number: 
+            print("Try again")
+    
 
 def main_menu():
     keep_running = "y"
 
     while keep_running == "y":
         
+        game()
 
-        print("Would you like to keep running?")
+        print("Would you like to play again?")
         keep_running = input("Y to keep running or any other button to stop")
 
         keep_running = keep_running.lower()
 
-def game():
-    question_b.get_random_number()
+        if keep_running != "y":
+            print("See you next time!")
+
 
 
 
 def main():
-    pass
+    main_menu()
